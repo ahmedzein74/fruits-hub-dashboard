6ebd94afbba5539a0743638a7f32872e3c900aba
Ahmed Zein <120061135+ahmedzein74@users.noreply.github.com>
2024-12-20 14:02:32 +0200

    Update Android SDK versions and add AppBar to AddProductView

--- lib/feature/add_proudct/presentation/views/add_proudct_view.dart
+++ lib/feature/add_proudct/presentation/views/add_proudct_view.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub_dashboard/feature/add_proudct/presentation/views/widgets/add_proudct_view_body.dart';
 
+import 'widgets/build_app_bar.dart';
+
 class AddProductView extends StatelessWidget {
   const AddProductView({super.key});
 
   static const routeName = 'AddProudctView';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: AddProudctViewBody()),
+    return Scaffold(
+      appBar: buildAppBar(title: 'Add Product'),
+      body: const SafeArea(child: AddProudctViewBody()),
     );
   }
 }
--- lib/feature/add_proudct/presentation/views/widgets/add_proudct_view_body.dart
+++ lib/feature/add_proudct/presentation/views/widgets/add_proudct_view_body.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub_dashboard/core/widgets/custom_text_form_field.dart';
+import 'package:fruits_hub_dashboard/feature/add_proudct/presentation/views/widgets/image_field.dart';
 
-class AddProudctViewBody extends StatelessWidget {
+class AddProudctViewBody extends StatefulWidget {
   const AddProudctViewBody({super.key});
 
   @override
+  State<AddProudctViewBody> createState() => _AddProudctViewBodyState();
+}
+
+class _AddProudctViewBodyState extends State<AddProudctViewBody> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: SingleChildScrollView(
+        child: Form(
+          autovalidateMode: autovalidateMode,
+          key: formKey,
+          child: const Column(
+            children: [
+              CustomTextFormField(
+                hintText: 'product name',
+                textInputType: TextInputType.text,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              CustomTextFormField(
+                hintText: 'product price',
+                textInputType: TextInputType.number,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              CustomTextFormField(
+                hintText: 'produt code',
+                textInputType: TextInputType.number,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              CustomTextFormField(
+                hintText: 'product description',
+                textInputType: TextInputType.text,
+                maxLines: 5,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              ImageField(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
